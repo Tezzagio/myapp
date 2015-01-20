76a901ab7271a3c1501ddead3fee5fcd16513889
Terry <tezorobert@hotmail.com>
2015-01-20 23:07:52 +0000

    added new content / portfolio content / nav bar

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,4 +1,12 @@
 class PortfolioController < ApplicationController
+	before_action :authenticate_user!, only: [ :new, :create ]
+
+
   def index
+  	@Portfolio = Portfolio.all
+  end
+
+  def IxD
+  	@Interaction_Design = Portfolio.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  get 'portfolio/index'
+
+  devise_scope :user do
+  get '/Logout' => 'devise/session#delete', as: 'Logout'
+end
+
+  root 'portfolio#index'
 
   devise_for :users
+
+
+  get '/portfolio' => 'portfolio#IxD'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
